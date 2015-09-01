92b47427746ec46a9e4d9241894b70cf31094e8f
Paul Wais <paulwais@gmail.com>
2015-09-01 13:27:27 -0700

    clean up, docs

---
+++ test-suite/java/docker/README.md
@@ -0,0 +1,29 @@
+Djinni Linux Tests
+------------------
+
+This directory contains a suite of tools for testing djinni (JNI only)
+on Linux.  The suite helps ensure the portability of djinni and 
+(self-)document compatible platforms.
+
+Quickstart
+----------
+
+You need to have [Docker](https://docker.com) installed.  To execute all
+tests, run `./test-suite/java/docker/linux_test.py` from the root of the
+djinni repository.
+
+Adding a Platform
+-----------------
+
+ 1. Create a subdirectory named after the platform (e.g. `my_platform`).
+ 2. If you can create a Docker image for your platform,
+    simply create a `Dockerfile` in the subdirectory that:
+    * Builds an image with djinni dependencies (e.g. Java, a C++ compiler)
+    * Expects djinni in `/opt/djinni/`
+    * Has a `CMD` to runs the JNI unit tests (and clean the `/build`
+        directory upon succcess assuming the tests are run in-place)
+ 3. You can test the new platform using:
+     `./test-suite/java/docker/linux_test.py ./test-suite/java/docker/my_platform`
+
+
+
--- test-suite/java/docker/linux_test.py
+++ test-suite/java/docker/linux_test.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# This script helps build and run djinni JNI tests
+# inside Docker containers.  FMI see README.md in
+# this directory.
+
 import logging
 import os
 import subprocess
@@ -16,21 +20,24 @@ def run_in_shell(cmd):
   log.info("... done")
 
 if __name__ == '__main__':
-  BASE_PATH = os.path.abspath("test-suite/java/docker")
+  DJINNI_ROOT = os.path.abspath(".")
+  BASE_PATH = os.path.join(DJINNI_ROOT, "test-suite/java/docker")
   assert \
     os.path.exists(os.path.join(BASE_PATH, "linux_test.py")), \
     "Please run this script from the root djinni directory"
 
+  # Support running a single image or all images
   image_dirs = (
-    sys.argv[1:] or 
+    sys.argv[1:] or
 	list(os.path.join(BASE_PATH, entry) for entry in os.listdir(BASE_PATH)))
   for im_dir in image_dirs:
     if not os.path.exists(os.path.join(im_dir, "Dockerfile")):
+      # We only support Docker-based images for now
       continue
 
     im_name = "djinni_test." + os.path.split(im_dir)[-1]
     run_in_shell("cd " + im_dir + " && docker build -t " + im_name + " .")
     run_in_shell(
-      "docker run -v " + os.path.abspath(".") + ":/opt/djinni " + im_name)
+      "docker run -v " + DJINNI_ROOT + ":/opt/djinni " + im_name)
 
 
