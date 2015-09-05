7a7aa0e5c3deb08ec550b1d9ceb689797b592055
Paul Wais <paulwais@gmail.com>
2015-09-05 15:24:55 -0700

    callgrind suggests that string translation is *very slow* (lol)

--- support-lib/java/com/dropbox/djinni/NativeLibLoader.java
+++ support-lib/java/com/dropbox/djinni/NativeLibLoader.java
@@ -1,11 +1,11 @@
-//                                                                                                                                                                                     
+//                                                                                                                                                                                                                                                                                                                                                                         
 // Copyright 2015 Dropbox, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+//        http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -34,7 +34,7 @@ import java.util.stream.Stream;
 
 /**
  * Utilities for loading native libraries containing djinni interfaces
- * and records.  To load libraries in your application at startup,
+ * and records. To load libraries in your application at startup,
  * simply place them:
  *  * inside the jar containing this code at the (jar-relative)
  *      path `djinniNativeLibsJarPath`
@@ -43,127 +43,132 @@ import java.util.stream.Stream;
  *
  * Then execute:
  * <code>
- *   NativeLibLoader.loadLibs()
+ *     NativeLibLoader.loadLibs()
  * </code>
  * to load the libraries (e.g. in your main() or a class static initializer). 
  */
 public class NativeLibLoader {
-  
-  /**
-   * Canonical directory in a jar containing (djinni-adapted) native libraries
-   */
-  public static final String djinniNativeLibsJarPath =
-      "resources/djinni_native_libs";
-  
-  /**
-   * Load native libraries with djinni support from the (comma-separated)
-   * path(s) provided in this system property 
-   */
-  public static final String djinniNativeLibsSysProp =
-      "djinni.native_libs_dirs";
-  
-  private static final Logger log =
-      Logger.getLogger(NativeLibLoader.class.getName());
-  
-  private NativeLibLoader() { } 
-  
-  // Load native libs from canonical locations
-  public static void loadLibs() throws URISyntaxException, IOException {
-    // Try to load from Jar
-    loadLibsFromJarPath(djinniNativeLibsJarPath);
     
-    // Try to load from system
-    String localPaths = System.getProperty(djinniNativeLibsSysProp);
-    if (localPaths != null) {
-      log.log(Level.FINE, "Loading local native libs");
-      for (String localPath : localPaths.split(",")) {
-        loadLibsFromLocalPath(Paths.get(localPath));
-      }
-    }
-  }
-  
-  // Load native lib(s) from the given `localPath` - a file or directory
-  public static void loadLibsFromLocalPath(Path localPath) throws IOException {
-    File localFile = localPath.toFile();
-    if (!localFile.exists()) { return; }
-    if (localFile.isDirectory()) {
-      log.log(Level.FINE, "Loading all libs in " + localFile.getAbsolutePath());
-      Stream<Path> streamPaths = Files.walk(localFile.toPath(), 1);
-      for (Path p : (Iterable<Path>)streamPaths::iterator) {
-        File f = p.toFile();
-        if (f.isFile()) {
-          loadLibrary(f.getAbsolutePath());
+    /**
+     * Canonical directory in a jar containing (djinni-adapted) native libraries
+     */
+    public static final String djinniNativeLibsJarPath =
+            "resources/djinni_native_libs";
+    
+    /**
+     * Load native libraries with djinni support from the (comma-separated)
+     * path(s) provided in this system property 
+     */
+    public static final String djinniNativeLibsSysProp =
+            "djinni.native_libs_dirs";
+    
+    private static final Logger log =
+            Logger.getLogger(NativeLibLoader.class.getName());
+    
+    private NativeLibLoader() { } 
+    
+    // Load native libs from canonical locations
+    public static void loadLibs() throws URISyntaxException, IOException {
+        // Try to load from Jar
+        loadLibsFromJarPath(djinniNativeLibsJarPath);
+        
+        // Try to load from system
+        String localPaths = System.getProperty(djinniNativeLibsSysProp);
+        if (localPaths != null) {
+            log.log(Level.FINE, "Loading local native libs");
+            for (String localPath : localPaths.split(",")) {
+                loadLibsFromLocalPath(Paths.get(localPath));
+            }
         }
-      }
-      streamPaths.close();
-    } else {
-      loadLibrary(localFile.getAbsolutePath());
     }
-  }
-  
-  // Load a directory of libs from a jar resource path
-  public static void loadLibsFromJarPath(String jarPath)
-      throws URISyntaxException, IOException {
+    
+    // Load native lib(s) from the given `localPath` - a file or directory
+    public static void loadLibsFromLocalPath(Path localPath) throws IOException {
+        File localFile = localPath.toFile();
+        if (!localFile.exists()) { return; }
+        if (localFile.isDirectory()) {
+            log.log(Level.FINE, "Loading all libs in " + localFile.getAbsolutePath());
+            Stream<Path> streamPaths = Files.walk(localFile.toPath(), 1);
+            for (Path p : (Iterable<Path>)streamPaths::iterator) {
+                File f = p.toFile();
+                if (f.isFile()) {
+                    loadLibrary(f.getAbsolutePath());
+                }
+            }
+            streamPaths.close();
+        } else {
+            loadLibrary(localFile.getAbsolutePath());
+        }
+    }
+    
+    // Load a directory of libs from a jar resource path
+    public static void loadLibsFromJarPath(String jarPath)
+            throws URISyntaxException, IOException {
 
-    // Do we have a valid path?
-    URL libsURL =
-        NativeLibLoader.class
-          .getClassLoader()
-          .getResource(djinniNativeLibsJarPath);
-    if (libsURL == null) { return; }
-    
-    // Are we actually referencing a jar path?
-    if (!libsURL.toURI().getScheme().equals("jar")) { return; }
-    
-    log.log(Level.FINE, "Loading libs from jar path " + jarPath);
-    
-    // Walk the directory and load libs
-    FileSystem fs =
-        FileSystems.newFileSystem(libsURL.toURI(), Collections.emptyMap());
-    Path myPath = fs.getPath(jarPath);
-    for (Path p : (Iterable<Path>)Files.walk(myPath, 1)::iterator) {
-      loadLibFromJarPath(p);
+        // Do we have a valid path?
+        URL libsURL =
+                NativeLibLoader.class
+                    .getClassLoader()
+                    .getResource(djinniNativeLibsJarPath);
+        if (libsURL == null) { return; }
+        
+        // Are we actually referencing a jar path?
+        if (!libsURL.toURI().getScheme().equals("jar")) { return; }
+        
+        log.log(Level.FINE, "Loading libs from jar path " + jarPath);
+        
+        // Walk the directory and load libs
+        FileSystem fs =
+                FileSystems.newFileSystem(libsURL.toURI(), Collections.emptyMap());
+        Path myPath = fs.getPath(jarPath);
+        for (Path p : (Iterable<Path>)Files.walk(myPath, 1)::iterator) {
+            loadLibFromJarPath(p);
+        }
+        
+        fs.close();
     }
     
-    fs.close();
-  }
-  
-  // Load a single native lib from a jar resource with path `libPath`
-  public static void loadLibFromJarPath(Path libPath) throws IOException {
+    // Load a single native lib from a jar resource with path `libPath`
+    public static void loadLibFromJarPath(Path libPath) throws IOException {
 
-    /*
-     * System libraries *must* be loaded from the filesystem,
-     * so copy the lib's data to a tempfile
-     */ 
-    InputStream libIn =
-        NativeLibLoader.class.getResourceAsStream(libPath.toString());
-    if (libIn == null) { return; }
-    File tempLib =
-        File.createTempFile(
-            libPath.getName(libPath.getNameCount() - 1).toString(),
-              // name tempfile after the lib to ease debugging
-            null);
-    tempLib.deleteOnExit();
-    
-    log.log(
-        Level.FINE,
-        "Copying jar lib " + libPath + " to " + tempLib.getAbsolutePath());
-    try {
-      Files.copy(libIn, tempLib.toPath(), StandardCopyOption.REPLACE_EXISTING);
-    } catch (SecurityException e) { 
-      throw new RuntimeException(
-          "SecurityException while trying to create tempfile: " +
-            e.getMessage() + "\n\n If you cannot grant this process " +
-            "permissions to create temporary files, you need to install " +
-            "the native libraries manually and provide the installation " + 
-            "path(s) using the system property " + djinniNativeLibsSysProp);
+        // System libraries *must* be loaded from the filesystem,
+        // so we must copy the lib's data from the jar to a tempfile
+
+        InputStream libIn =
+            NativeLibLoader.class.getResourceAsStream(libPath.toString());
+        if (libIn == null) { return; } // Invalid `libPath`
+        
+        // Name the tempfile
+        String libName = libPath.getName(libPath.getNameCount() - 1).toString();
+            // Name the tempfile after the lib to ease debugging
+        String suffix = null;
+        int extPos = libName.lastIndexOf('.');
+        if (extPos > 0) { suffix = libName.substring(extPos + 1); }
+            // Try to suffix the tempfile with the lib's suffix so that other
+            // tools (e.g. profilers) identify the file correctly
+        
+        File tempLib = File.createTempFile(libName, suffix);
+        tempLib.deleteOnExit();
+        
+        log.log(
+            Level.FINE,
+            "Copying jar lib " + libPath + " to " + tempLib.getAbsolutePath());
+        try {
+            Files.copy(libIn, tempLib.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (SecurityException e) { 
+            throw new RuntimeException(
+                    "SecurityException while trying to create tempfile: " +
+                        e.getMessage() + "\n\n If you cannot grant this process " +
+                        "permissions to create temporary files, you need to install " +
+                        "the native libraries manually and provide the installation " + 
+                        "path(s) using the system property " + djinniNativeLibsSysProp);
+        }
+        
+        loadLibrary(tempLib.getAbsolutePath());
     }
     
-    loadLibrary(tempLib.getAbsolutePath());
-  }
-  
-  private static void loadLibrary(String abspath) {
-    System.load(abspath);
-    log.log(Level.INFO, "Loaded " + abspath);
-  }
+    private static void loadLibrary(String abspath) {
+        System.load(abspath);
+        log.log(Level.INFO, "Loaded " + abspath);
+    }
 }
