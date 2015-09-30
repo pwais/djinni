31ca907dc5bd5d6cfe6bcf23663fa6f8325cf554
Paul Wais <paulwais@gmail.com>
2015-09-30 00:48:51 -0700

    make YamlGenerator not barf on mising languages.  really really stupid bugs and code here.  also removed tabs

--- src/source/YamlGenerator.scala
+++ src/source/YamlGenerator.scala
@@ -8,6 +8,7 @@ import djinni.writer.IndentWriter
 import java.util.{Map => JMap}
 import scala.collection.JavaConversions._
 import scala.collection.mutable
+import scala.collection.immutable.{Map => IMap}
 
 class YamlGenerator(spec: Spec) extends Generator(spec) {
 
@@ -40,59 +41,59 @@ class YamlGenerator(spec: Spec) extends Generator(spec) {
   }
 
   private def writeYamlFile(ident: String, origin: String, td: InternTypeDecl): Unit =
-  	writeYamlFile(spec.yamlPrefix + ident + ".yaml", origin, w => {
+    writeYamlFile(spec.yamlPrefix + ident + ".yaml", origin, w => {
       write(w, td)
-	})
+    })
 
   private def write(w: IndentWriter, td: TypeDecl) {
-  	write(w, preamble(td))
-  	w.wl("cpp:").nested { write(w, cpp(td)) }
-  	w.wl("objc:").nested { write(w, objc(td)) }
-  	w.wl("objcpp:").nested { write(w, objcpp(td)) }
-  	w.wl("java:").nested { write(w, java(td)) }
-  	w.wl("jni:").nested { write(w, jni(td)) }
+    write(w, preamble(td))
+    w.wl("cpp:").nested { write(w, cpp(td)) }
+    w.wl("objc:").nested { write(w, objc(td)) }
+    w.wl("objcpp:").nested { write(w, objcpp(td)) }
+    w.wl("java:").nested { write(w, java(td)) }
+    w.wl("jni:").nested { write(w, jni(td)) }
   }
 
   private def write(w: IndentWriter, m: Map[String, Any]) {
-  	for((k, v) <- m) {
-  	  w.w(k + ": ")
-  	  v match {
-  	  	case s: String => write(w, s)
-  	  	case s: QuotedString => write(w, s)
-  	  	case m: Map[_, _] => w.wl.nested { write(w, m.asInstanceOf[Map[String, Any]]) }
-  	  	case s: Seq[_] => write(w, s)
-  	  	case b: Boolean => write(w, b)
-  	  	case _ => throw new AssertionError("unexpected map value")
-  	  }
-  	}
+    for((k, v) <- m) {
+      w.w(k + ": ")
+      v match {
+        case s: String => write(w, s)
+        case s: QuotedString => write(w, s)
+        case m: Map[_, _] => w.wl.nested { write(w, m.asInstanceOf[Map[String, Any]]) }
+        case s: Seq[_] => write(w, s)
+        case b: Boolean => write(w, b)
+        case _ => throw new AssertionError("unexpected map value")
+      }
+    }
   }
 
   private def write(w: IndentWriter, s: Seq[Any]) {
-  	// The only arrays we have are small enough to use flow notation
-  	w.wl(s.mkString("[", ",", "]"))
+    // The only arrays we have are small enough to use flow notation
+    w.wl(s.mkString("[", ",", "]"))
   }
 
   private def write(w: IndentWriter, b: Boolean) {
-  	w.wl(if(b) "true" else "false")
+    w.wl(if(b) "true" else "false")
   }
 
   private def write(w: IndentWriter, s: String) {
-  	if(s.isEmpty) w.wl(q("")) else w.wl(s)
+    if(s.isEmpty) w.wl(q("")) else w.wl(s)
   }
 
   private def write(w: IndentWriter, s: QuotedString) {
-  	if(s.str.isEmpty) w.wl(q("")) else w.wl("'" + s.str.replaceAllLiterally("'", "''") + "'")
+    if(s.str.isEmpty) w.wl(q("")) else w.wl("'" + s.str.replaceAllLiterally("'", "''") + "'")
   }
 
   private def preamble(td: TypeDecl) = Map[String, Any](
     "name" -> (spec.yamlPrefix + td.ident.name),
     "typedef" -> QuotedString(typeDef(td)),
-  	"params" -> td.params.collect { case p: TypeParam => p.ident.name },
+    "params" -> td.params.collect { case p: TypeParam => p.ident.name },
     "prefix" -> spec.yamlPrefix
   )
 
   private def typeDef(td: TypeDecl) = {
-  	def ext(e: Ext): String = (if(e.cpp) " +c" else "") + (if(e.objc) " +o" else "") + (if(e.java) " +j" else "")
+    def ext(e: Ext): String = (if(e.cpp) " +c" else "") + (if(e.objc) " +o" else "") + (if(e.java) " +j" else "")
     def deriving(r: Record) = {
       if(r.derivingTypes.isEmpty) {
         ""
@@ -103,11 +104,11 @@ class YamlGenerator(spec: Spec) extends Generator(spec) {
         }.mkString(" deriving(", ", ", ")")
       }
     }
-  	td.body match {
-  	  case i: Interface => "interface" + ext(i.ext)
-  	  case r: Record => "record" + ext(r.ext) + deriving(r)
-  	  case e: Enum => "enum"
-  	}
+    td.body match {
+      case i: Interface => "interface" + ext(i.ext)
+      case r: Record => "record" + ext(r.ext) + deriving(r)
+      case e: Enum => "enum"
+    }
   }
 
   private def cpp(td: TypeDecl) = Map[String, Any](
@@ -138,8 +139,8 @@ class YamlGenerator(spec: Spec) extends Generator(spec) {
   )
 
   private def jni(td: TypeDecl) = Map[String, Any](
-  	"translator" -> QuotedString(jniMarshal.helperName(mexpr(td))),
-  	"header" -> QuotedString(jniMarshal.include(td.ident)),
+    "translator" -> QuotedString(jniMarshal.helperName(mexpr(td))),
+    "header" -> QuotedString(jniMarshal.include(td.ident)),
     "typename" -> jniMarshal.fqParamType(mexpr(td)),
     "typeSignature" -> QuotedString(jniMarshal.fqTypename(td.ident, td.body))
   )
@@ -148,77 +149,87 @@ class YamlGenerator(spec: Spec) extends Generator(spec) {
   private def mexpr(td: TypeDecl) = MExpr(meta(td), List())
 
   private def meta(td: TypeDecl) = {
-  	val defType = td.body match {
-  		case i: Interface => DInterface
-  		case r: Record => DRecord
-  		case e: Enum => DEnum
-  	}
-  	MDef(td.ident, 0, defType, td.body)
+    val defType = td.body match {
+        case i: Interface => DInterface
+        case r: Record => DRecord
+        case e: Enum => DEnum
+    }
+    MDef(td.ident, 0, defType, td.body)
   }
 
   override def generate(idl: Seq[TypeDecl]) {
-  	val internOnly = idl.collect { case itd: InternTypeDecl => itd }.sortWith(_.ident.name < _.ident.name)
-  	if(spec.yamlOutFile.isDefined) {
-  	  writeYamlFile(internOnly)
-  	} else {
-  	  for(td <- internOnly) {
-  	    writeYamlFile(td.ident, td.origin, td)
-  	  }
-  	}
+    val internOnly = idl.collect { case itd: InternTypeDecl => itd }.sortWith(_.ident.name < _.ident.name)
+    if(spec.yamlOutFile.isDefined) {
+      writeYamlFile(internOnly)
+    } else {
+      for(td <- internOnly) {
+        writeYamlFile(td.ident, td.origin, td)
+      }
+    }
   }
 
   override def generateEnum(origin: String, ident: Ident, doc: Doc, e: Enum) {
-  	// unused
+    // unused
   }
 
   override def generateInterface(origin: String, ident: Ident, doc: Doc, typeParams: Seq[TypeParam], i: Interface) {
-  	// unused
+    // unused
   }
 
   override def generateRecord(origin: String, ident: Ident, doc: Doc, params: Seq[TypeParam], r: Record) {
-  	// unused
+    // unused
   }
 }
 
 object YamlGenerator {
   def metaFromYaml(td: ExternTypeDecl) = MExtern(
     td.ident.name.stripPrefix(td.properties("prefix").toString), // Make sure the generator uses this type with its original name for all intents and purposes
-  	td.params.size,
-  	defType(td),
-  	td.body,
-    MExtern.Cpp(
-      nested(td, "cpp")("typename").toString,
-      nested(td, "cpp")("header").toString,
-      nested(td, "cpp")("byValue").asInstanceOf[Boolean]),
-    MExtern.Objc(
-      nested(td, "objc")("typename").toString,
-      nested(td, "objc")("header").toString,
-      nested(td, "objc")("boxed").toString,
-      nested(td, "objc")("pointer").asInstanceOf[Boolean],
-      nested(td, "objc")("hash").toString),
-    MExtern.Objcpp(
-      nested(td, "objcpp")("translator").toString,
-      nested(td, "objcpp")("header").toString),
-    MExtern.Java(
-      nested(td, "java")("typename").toString,
-      nested(td, "java")("boxed").toString,
-      nested(td, "java")("reference").asInstanceOf[Boolean],
-      nested(td, "java")("generic").asInstanceOf[Boolean],
-      nested(td, "java")("hash").toString),
-    MExtern.Jni(
-      nested(td, "jni")("translator").toString,
-      nested(td, "jni")("header").toString,
-      nested(td, "jni")("typename").toString,
-      nested(td, "jni")("typeSignature").toString)
+    td.params.size,
+    defType(td),
+    td.body,
+    toMExtern(td, "cpp", (props: Map[String, Any]) => 
+                            MExtern.Cpp(
+                              props("typename").toString,
+                              props("header").toString,
+                              props("byValue").asInstanceOf[Boolean])),
+    toMExtern(td, "objc", (props: Map[String, Any]) => 
+                            MExtern.Objc(
+                              props("typename").toString,
+                              props("header").toString,
+                              props("boxed").toString,
+                              props("pointer").asInstanceOf[Boolean],
+                              props("hash").toString)),
+    toMExtern(td, "objcpp", (props: Map[String, Any]) =>
+                            MExtern.Objcpp(
+                              props("translator").toString,
+                              props("header").toString)),
+    toMExtern(td, "java", (props: Map[String, Any]) =>
+                            MExtern.Java(
+                              props("typename").toString,
+                              props("boxed").toString,
+                              props("reference").asInstanceOf[Boolean],
+                              props("generic").asInstanceOf[Boolean],
+                              props("hash").toString)),
+    toMExtern(td, "jni", (props: Map[String, Any]) =>
+                            MExtern.Jni(
+                              props("translator").toString,
+                              props("header").toString,
+                              props("typename").toString,
+                              props("typeSignature").toString))
   )
 
-  private def nested(td: ExternTypeDecl, key: String) = {
-  	td.properties.get(key).collect { case m: JMap[_, _] => m.collect { case (k: String, v: Any) => (k, v) } } getOrElse(Map[String, Any]())
+  private def toMExtern[T >: Null](td: ExternTypeDecl, key: String, ctor: (Map[String, Any] => T)) : T = {
+    td.properties.get(key) match {
+      case Some(rawProps: JMap[_, _]) => {
+        ctor(rawProps.map(kv => (kv._1.toString, kv._2)).toMap)
+      }
+      case _ => null
+    }
   }
 
   private def defType(td: ExternTypeDecl) = td.body match {
-  	case i: Interface => DInterface
-  	case r: Record => DRecord
-  	case e: Enum => DEnum
+    case i: Interface => DInterface
+    case r: Record => DRecord
+    case e: Enum => DEnum
   }
 }
